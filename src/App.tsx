72aea14e7a316c74862425a202d0e40b41256a31
Emil <kamiekurat98@o2.pl>
2022-09-11 10:50:10 +0200

    Installed react-router-dom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+      <div className="App">
+        <BrowserRouter>
+          <Route/>
+        </BrowserRouter>
+      </div>
+  );
 }
 
 export default App;
