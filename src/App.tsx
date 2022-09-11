5ec6db7f1accc6d3cac27cd067964d245713d044
Emil <kamiekurat98@o2.pl>
2022-09-11 19:01:16 +0200

    Added login html element and style.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route} from "react-router-dom";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
       <div className="App">
         <BrowserRouter>
-          <Route/>
+            <Route path={"/"} exact component={Users} />
+            <Route path={"/login"} component={Login} />
+            <Route path={"/register"} component={Register} />
         </BrowserRouter>
       </div>
   );
