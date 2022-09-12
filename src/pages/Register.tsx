86db4881b202019d8f37677e780efe0473e9be15
Emil <kamiekurat98@o2.pl>
2022-09-12 21:53:18 +0200

    Added register html element.

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, {Component, SyntheticEvent, useState} from 'react';
+import axios from 'axios';
 
 const Register = () => {
-  return <div></div>;
-};
+    let firstName = '';
+    let lastName = '';
+    let email = '';
+    let password = '';
+    let passwordConfirm = '';
+
+    let val = false;
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+       await axios.post('http://localhost/api/admin/register', {
+           firstName: firstName,
+           lastName,
+           email,
+           password,
+           passwordConfirm
+       });
+        val = true
+    }
+
+
+        return (
+            <main className="form-signin">
+                <form onSubmit={submit}>
+                    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+
+                    <div className="form-floating">
+                        <input className="form-control" placeholder="First name" onChange={e => firstName = e.target.value}/>
+                        <label>First name</label>
+                    </div>
+
+                    <div className="form-floating">
+                        <input className="form-control" placeholder="Last name" onChange={e => lastName = e.target.value}/>
+                        <label>Last name</label>
+                    </div>
+
+                    <div className="form-floating">
+                        <input type="email" className="form-control" placeholder="name@example.com" onChange={e => email = e.target.value}/>
+                        <label>Email address</label>
+                    </div>
+
+                    <div className="form-floating">
+                        <input type="password" className="form-control" placeholder="Password" onChange={e => password = e.target.value}/>
+                        <label>Password</label>
+                    </div>
+
+                    <div className="form-floating">
+                        <input type="password" className="form-control" placeholder="Password Confirm" onChange={e => passwordConfirm = e.target.value}/>
+                        <label>Password Confirm</label>
+                    </div>
+
+                    <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+                </form>
+            </main>
+        );
+
+}
 
 export default Register;
