6b2fb2da0759732cf0830c36825114abd886f0a0
Emil <kamiekurat98@o2.pl>
2022-12-12 23:12:19 +0100

    Created layout spare parts form.

--- src/pages/spare-parts/SparePartsForm.tsx
+++ src/pages/spare-parts/SparePartsForm.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Layout from "../../components/Layout";
+import {Button, TextField} from "@material-ui/core";
 
 const SparePartsForm = () => {
     return (
-        <div>
-            
-        </div>
+        <Layout>
+            <form>
+                <div className="mb-3">
+                    <TextField label="Id"/>
+                </div>
+                <div className="mb-3">
+                    <TextField label="Title"/>
+                </div>
+                <div className="mb-3">
+                    <TextField label="Description"/>
+                </div>
+                <Button variant="contained" color="primary">Submit</Button>
+            </form>
+        </Layout>
     );
 };
 
