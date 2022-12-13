abe1f24c946ae63ae8c32928229324ee377db873
Emil <kamiekurat98@o2.pl>
2022-12-13 23:00:37 +0100

    Added variables and submit function.

--- src/pages/spare-parts/SparePartsForm.tsx
+++ src/pages/spare-parts/SparePartsForm.tsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, {SyntheticEvent, useState} from 'react';
 import Layout from "../../components/Layout";
 import {Button, TextField} from "@material-ui/core";
+import axios from "axios";
+import {Redirect} from "react-router-dom";
 
 const SparePartsForm = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [redirect, setRedirect] = useState(false);
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        await axios.post('spareParts', {
+            title,
+            description
+        });
+        setRedirect(true);
+    }
+
+    if (redirect) {
+        return <Redirect to={'/spareparts'}/>
+    }
+
     return (
         <Layout>
-            <form>
+            <form onSubmit={submit}>
                 <div className="mb-3">
-                    <TextField label="Id"/>
+                    <TextField label="Title" onChange={e=> setTitle(e.target.value)}/>
                 </div>
                 <div className="mb-3">
-                    <TextField label="Title"/>
+                    <TextField label="Description" onChange={e=>setDescription(e.target.value)}/>
                 </div>
-                <div className="mb-3">
-                    <TextField label="Description"/>
-                </div>
-                <Button variant="contained" color="primary">Submit</Button>
+                <Button variant="contained" color="primary" type="submit">Submit</Button>
             </form>
         </Layout>
     );
