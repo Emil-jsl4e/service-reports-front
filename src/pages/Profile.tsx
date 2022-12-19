992e4a5b4054d8895c4368e16273257e4917be7a
Emil <kamiekurat98@o2.pl>
2022-12-19 22:52:23 +0100

    Added layout to profile.

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import Layout from "../components/Layout";
+import {Button, TextField} from "@material-ui/core";
 
 const Profile = () => {
     return (
         <Layout>
-            
+            <h3>Account Information</h3>
+            <form>
+                <div className="mb-3">
+                    < TextField label="First Name"/>
+                </div>
+                <div className="mb-3">
+                    < TextField label="First Name"/>
+                </div>
+                <div className="mb-3">
+                    < TextField label="First Name"/>
+                </div>
+                <Button variant="contained" color="primary" type="submit">Submit</Button>
+            </form>
+            <h3 className="mt-4">Change Password</h3>
+            <form>
+                <div className="mb-3">
+                    < TextField label="Password" type="password"/>
+                </div>
+                <div className="mb-3">
+                    < TextField label="Password Confirm" type="password"/>
+                </div>
+                <Button variant="contained" color="primary" type="submit">Submit</Button>
+            </form>
         </Layout>
     );
 };
