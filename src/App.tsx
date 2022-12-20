f18edd3714e7d21feaa59cd7e28d150b72b981c5
Emil <kamiekurat98@o2.pl>
2022-12-20 22:46:48 +0100

    Added field set value and route profile.

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,24 @@ import RedirectToUsers from "./components/RedirectToUsers";
 import Links from "./pages/Links";
 import SpareParts from "./pages/spare-parts/SpareParts"
 import SparePartsForm from "./pages/spare-parts/SparePartsForm";
+import Profile from "./pages/Profile";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-          <Route path={'/'} exact component={RedirectToUsers}/>
-          <Route path={'/login'} component={Login} />
-          <Route path={'/register'} component={Register} />
-          <Route path={'/users'} exact component={Users} />
-          <Route path={'/users/:id/links'} component={Links} />
-          <Route path={'/spareparts'} exact component={SpareParts} />
-          <Route path={'/spareparts/create'} component={SparePartsForm} />
-          <Route path={'/spareparts/:id/edit'} component={SparePartsForm} />
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Route path={'/'} exact component={RedirectToUsers}/>
+                <Route path={'/login'} component={Login}/>
+                <Route path={'/register'} component={Register}/>
+                <Route path={'/profile'} component={Profile}/>
+                <Route path={'/users'} exact component={Users}/>
+                <Route path={'/users/:id/links'} component={Links}/>
+                <Route path={'/spareparts'} exact component={SpareParts}/>
+                <Route path={'/spareparts/create'} component={SparePartsForm}/>
+                <Route path={'/spareparts/:id/edit'} component={SparePartsForm}/>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -14,26 +14,23 @@ const Profile = () => {
             <h3>Account Information</h3>
             <form>
                 <div className="mb-3">
-                    < TextField label="First Name"/>
+                    < TextField label="First Name" onChange={e => setFirstName(e.target.value)}/>
                 </div>
                 <div className="mb-3">
-                    < TextField label="First Name"/>
+                    < TextField label="Last Name" onChange={e => setLastName(e.target.value)}/>
                 </div>
                 <div className="mb-3">
-                    < TextField label="First Name"/>
-                </div>
-                <div className="mb-3">
-                    < TextField label="Email"/>
+                    < TextField label="Email" onChange={e => setEmail(e.target.value)}/>
                 </div>
                 <Button variant="contained" color="primary" type="submit">Submit</Button>
             </form>
             <h3 className="mt-4">Change Password</h3>
             <form>
                 <div className="mb-3">
-                    < TextField label="Password" type="password"/>
+                    < TextField label="Password" type="password" onChange={e => setPassword(e.target.value)}/>
                 </div>
                 <div className="mb-3">
-                    < TextField label="Password Confirm" type="password"/>
+                    < TextField label="Password Confirm" type="password" onChange={e => setPasswordConfirm(e.target.value)}/>
                 </div>
                 <Button variant="contained" color="primary" type="submit">Submit</Button>
             </form>
