4089a583a0427599729bde6f57970dad08dfb880
Emil <kamiekurat98@o2.pl>
2022-12-20 22:38:34 +0100

    Created values, added email field and value.

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Layout from "../components/Layout";
 import {Button, TextField} from "@material-ui/core";
 
 const Profile = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+
     return (
         <Layout>
             <h3>Account Information</h3>
@@ -15,6 +21,9 @@ const Profile = () => {
                 </div>
                 <div className="mb-3">
                     < TextField label="First Name"/>
+                </div>
+                <div className="mb-3">
+                    < TextField label="Email"/>
                 </div>
                 <Button variant="contained" color="primary" type="submit">Submit</Button>
             </form>
