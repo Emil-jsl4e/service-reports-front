5986e32d7bb5305a1efb83c8b1c84676f4f466c8
Emil <kamiekurat98@o2.pl>
2022-12-21 22:25:02 +0100

    Added set function data and submit function.

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {SyntheticEvent, useEffect, useState} from 'react';
 import Layout from "../components/Layout";
 import {Button, TextField} from "@material-ui/core";
+import axios from "axios";
 
 const Profile = () => {
     const [firstName, setFirstName] = useState('');
@@ -9,23 +10,54 @@ const Profile = () => {
     const [password, setPassword] = useState('');
     const [passwordConfirm, setPasswordConfirm] = useState('');
 
+    useEffect(() =>{
+        (
+            async () => {
+                const {data} = await axios.get('user');
+
+                setFirstName(data.firstName);
+                setFirstName(data.lastName);
+                setFirstName(data.email);
+            }
+        )();
+    },[]);
+
+    const submit = async (e:SyntheticEvent) => {
+        e.preventDefault();
+
+        await axios.put('user/data', {
+            firstName,
+            lastName,
+            email
+        })
+    }
+
+    const submitPassword = async (e:SyntheticEvent) =>{
+        e.preventDefault();
+
+        await axios.put('user/password', {
+            password,
+            passwordConfirm
+        })
+    }
+
     return (
         <Layout>
             <h3>Account Information</h3>
-            <form>
+            <form onSubmit={submit}>
                 <div className="mb-3">
-                    < TextField label="First Name" onChange={e => setFirstName(e.target.value)}/>
+                    < TextField label="First Name" value={firstName} onChange={e => setFirstName(e.target.value)}/>
                 </div>
                 <div className="mb-3">
-                    < TextField label="Last Name" onChange={e => setLastName(e.target.value)}/>
+                    < TextField label="Last Name" value={lastName} onChange={e => setLastName(e.target.value)}/>
                 </div>
                 <div className="mb-3">
-                    < TextField label="Email" onChange={e => setEmail(e.target.value)}/>
+                    < TextField label="Email" value={email} onChange={e => setEmail(e.target.value)}/>
                 </div>
                 <Button variant="contained" color="primary" type="submit">Submit</Button>
             </form>
             <h3 className="mt-4">Change Password</h3>
-            <form>
+            <form onSubmit={submitPassword}>
                 <div className="mb-3">
                     < TextField label="Password" type="password" onChange={e => setPassword(e.target.value)}/>
                 </div>
